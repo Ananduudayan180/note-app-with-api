1e0b3953f5338314dead85d1c54027ed9977b1de
Anandu Udayan <anandhuudayan180@gamil.com>
2025-12-28 12:47:37 +0530

    feat: fetch and display notes list

--- lib/note_pages/note_list.dart
+++ lib/note_pages/note_list.dart
@@ -1,19 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/data/data.dart';
+import 'package:note_app/data/note_model/note_model.dart';
 import 'package:note_app/utils/app_colors.dart';
 
 class NoteList extends StatefulWidget {
-  const NoteList({super.key});
+  final Function? refreshNotes;
+  const NoteList({super.key, this.refreshNotes});
 
   @override
   State<NoteList> createState() => _NoteListState();
 }
 
 class _NoteListState extends State<NoteList> {
+  final List<NoteModel> notesList = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchNotes();
+  }
+
+  Future<void> fetchNotes() async {
+    final notes = await NoteDB().getAllNote();
+    setState(() {
+      notesList.clear();
+      notesList.addAll(notes.reversed);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final TextTheme textTheme = Theme.of(context).textTheme;
     return ListView.builder(
       itemBuilder: (context, index) {
+        final NoteModel note = notesList[index];
         return Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           child: GestureDetector(
@@ -30,9 +49,12 @@ class _NoteListState extends State<NoteList> {
                   horizontal: 12.0,
                   vertical: 4.0,
                 ),
-                title: Text('Note ${index + 1}', style: textTheme.titleMedium),
+                title: Text(
+                  note.title ?? 'No Title',
+                  style: textTheme.titleMedium,
+                ),
                 subtitle: Text(
-                  'This is sample content for note',
+                  note.content ?? 'No Content',
                   style: textTheme.bodyMedium,
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
@@ -49,7 +71,7 @@ class _NoteListState extends State<NoteList> {
           ),
         );
       },
-      itemCount: 10,
+      itemCount: notesList.length,
     );
   }
 }
