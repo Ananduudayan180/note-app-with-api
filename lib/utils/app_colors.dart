0efce19cf76fd3b9715b692fb125bafaa28cafd7
Anandu Udayan <anandhuudayan180@gamil.com>
2026-01-01 11:19:28 +0530

    feat: add reusable utils for colors and appStr

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appThemeColor = Color(0xFF1F1F1F);
+  static const Color appContainerColor = Colors.black12;
+}
---
+++ lib/utils/app_str.dart
@@ -0,0 +1,6 @@
+class AppStr {
+  static const String notes = 'NOTES';
+  static const String noteTitle = 'Title';
+  static const String noteDescription = 'Description';
+  static const String addNote = 'ADD NOTE';
+}
